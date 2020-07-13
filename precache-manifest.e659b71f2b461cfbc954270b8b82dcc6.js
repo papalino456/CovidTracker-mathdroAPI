f9baf7d887094a7dfd84d1d9f7eb52e4c95bc796
papalino456 <sebastian.barrio.b@gmail.com>
2020-07-12 21:31:05 -0500

    Updates

---
+++ precache-manifest.e659b71f2b461cfbc954270b8b82dcc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa32d49d9dd069101de576757a9fb33",
+    "url": "/CovidTracker-mathdroAPI/index.html"
+  },
+  {
+    "revision": "33ac65612c091e57f175",
+    "url": "/CovidTracker-mathdroAPI/static/css/main.9e74d962.chunk.css"
+  },
+  {
+    "revision": "0be8aab7e1112569d619",
+    "url": "/CovidTracker-mathdroAPI/static/js/2.5cfb0d31.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker-mathdroAPI/static/js/2.5cfb0d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ac65612c091e57f175",
+    "url": "/CovidTracker-mathdroAPI/static/js/main.4bdbd550.chunk.js"
+  },
+  {
+    "revision": "da2a1d71b4439b9f7095",
+    "url": "/CovidTracker-mathdroAPI/static/js/runtime-main.f4de1af1.js"
+  },
+  {
+    "revision": "8f51f8acd0e7ccfa2233315d55e7f915",
+    "url": "/CovidTracker-mathdroAPI/static/media/logo.8f51f8ac.png"
+  }
+]);
